fc419325f2523688e685fc0a448c03cf684c5c0c
devayani03 <drchandane_b21@ce.vjti.ac.in>
2023-10-24 19:12:07 +0530

    changes

--- se_project/home.py
+++ se_project/home.py
@@ -1,36 +1,55 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt5.QtCore import QFile, QTextStream
 
-
-class BasicWebsite(QMainWindow):
+class CrimeInvestigationWebsite(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Basic Website with PyQt")
-        self.setGeometry(100, 100, 400, 100)
+        self.setWindowTitle("Crime Investigation Website")
+        self.setGeometry(100, 100, 800, 600)
 
-        # Create a central widget and a layout
+        self.load_css("styles.css")
+
+
+
+        # Create a central widget and layout
         central_widget = QWidget()
-        layout = QVBoxLayout(central_widget)
-
-        # Create a button to open the website
-        open_button = QPushButton("Open Website in Browser")
-        open_button.clicked.connect(self.open_website)
-
-        layout.addWidget(open_button)
         self.setCentralWidget(central_widget)
 
-    def open_website(self):
-        # Specify the URL you want to open in the user's default web browser
-        url = QUrl("https://www.example.com")
+        # Create a navigation bar (horizontal layout)
+        navbar = QHBoxLayout()
 
-        # Use QDesktopServices to open the URL in the default web browser
-        QDesktopServices.openUrl(url)
+        # Buttons for navigation
+        home_button = QPushButton("Home")
+        registry_upload_button = QPushButton("Registry Upload")
+        malware_detection_button = QPushButton("Malware Detection")
+        analysis_options_button = QPushButton("Analysis Options")
 
+        # Add buttons to the navbar
+        navbar.addWidget(home_button)
+        navbar.addWidget(registry_upload_button)
+        navbar.addWidget(malware_detection_button)
+        navbar.addWidget(analysis_options_button)
+
+        # Label for website description
+        description_label = QLabel("This website will help the forensic department find cyber criminals.")
+
+        # Create a vertical layout for the content
+        content_layout = QVBoxLayout()
+        content_layout.addLayout(navbar)
+        content_layout.addWidget(description_label)
+
+        central_widget.setLayout(content_layout)
+
+    def load_css(self, file_name):
+        file = QFile(file_name)
+        if file.open(QFile.ReadOnly | QFile.Text):
+            stream = QTextStream(file)
+            self.setStyleSheet(stream.readAll())
+            file.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = BasicWebsite()
+    window = CrimeInvestigationWebsite()
     window.show()
     sys.exit(app.exec_())
